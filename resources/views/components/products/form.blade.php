2b900a615cc3f3561ef438c9cf7f4ade1d93a727
Andreawingardh <andreawingardh@gmail.com>
2025-02-27 15:14:06 +0100

    Updated form to show existing data from database

--- resources/views/components/products/form.blade.php
+++ resources/views/components/products/form.blade.php
@@ -1,24 +1,24 @@
 <label for='name'>Name</label>
-<input type='text' name='name' value='{{ old('name') }}'>
+<input type='text' name='name' value='{{ old('name', $product->name ?? '') }}'>
 
 <label for='description'>Description</label>
-<textarea name='description'>{{ old('description') }}</textarea>
+<textarea name='description'>{{old('description', $product->description ?? '') }}</textarea>
 
 <label for='price'>Price</label>
-<input type='text' name='price' value='{{ old('price') }}'>
+<input type='text' name='price' value='{{ old('price', $product->price ?? '') }}'>
 <label for='brand'>Brand</label>
-<input type='text' name='brand' value='{{ old('brand') }}'>
+<input type='text' name='brand' value='{{ old('brand', $product->brand ?? '') }}'>
 
 <fieldset>
 <legend for='dimensions'>Dimensions</legend>
 <label for='height'>Height</label>
-<input type='text' name='height' value='{{ old('height') }}'>
+<input type='text' name='height' value='{{ old('height', $product->height ?? '') }}'>
 <label for='width'>Width</label>
-<input type='text' name='width' value='{{ old('width') }}'>
+<input type='text' name='width' value='{{ old('width', $product->width ?? '') }}'>
 </fieldset>
 
 <label for='weight'>Weight</label>
-<input type='text' name='weight' value='{{ old('weight') }}'>
+<input type='text' name='weight' value='{{ old('weight', $product->weight ?? '') }}'>
 
 <label for='categories'>Categories</label>
 <select name='categories[]' multiple>
