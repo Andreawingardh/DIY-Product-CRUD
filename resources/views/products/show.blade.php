072d2df03077f6c80962ca4f5df602701ffbdf2f
Andreawingardh <andreawingardh@gmail.com>
2025-02-27 15:15:06 +0100

    Updated show.view with links to products.edit and products.destroy

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,10 +1,18 @@
 <x-layout>
 
     <h2>{{$product->name }}</h2>
+    <a href="/products/{{$product->id}}/edit"><button>Edit</button></a>
     <h3>Brand: {{$product->brand->name}}</h3>
     <p>The product weighs {{$product->weight}}.</p>
     <p>Dimensions: {{$product->height}} x {{$product->width}}.</p>
     <p>Category: {{$product->category->name}}</p>
+
+    <form method="post" action="{{ route('products.destroy', $product)}}">
+    @csrf
+    @method('DELETE')
+    <button>DELETE</button>
+
+    </form>
 
     
 </x-layout>
