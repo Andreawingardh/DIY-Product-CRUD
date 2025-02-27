39f094475888a6c6392ebf15b55a4699670b2777
Andreawingardh <andreawingardh@gmail.com>
2025-02-27 13:22:19 +0100

    Added an index() function where send the data from the products table along with the data from category and brand

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Brand;
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -12,7 +14,8 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        $products = Product::with(['category', 'brand'])->get();
+        return view('products.index', ['products' => $products]);
     }
 
     /**
