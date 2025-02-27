4c2bc3d4301342140bd50745fa06fe601152eb7d
Andreawingardh <andreawingardh@gmail.com>
2025-02-27 15:13:48 +0100

    Created routes for: create, edit, created functions: update and destroy

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -21,23 +21,26 @@ class ProductController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create() {}
+    public function create()
+    {
 
-    /**
-     * Store a newly created resource in storage.
-     */
+        return view('products.create');
+        /**
+         * Store a newly created resource in storage.
+         */
+    }
     public function store(Request $request)
     {
         $request->validate([
 
             'name' => 'required|min:10|max:100',
             'description' => 'nullable|min:10',
-            'brand' => 'required|min:3',
+            // 'brand' => 'required|min:3',
             'price' => 'required|decimal:0,2',
             'height' => 'nullable|decimal:0,2',
             'width' => 'nullable|decimal:0,2',
             'weight' => 'nullable|decimal:0,2',
-            'category' => 'required|max:50',
+            // 'category' => 'required|max:50',
         ]);
 
         Product::create($request->input());
@@ -46,7 +49,7 @@ class ProductController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Product $product) 
+    public function show(Product $product)
     {
         return view('products.show', ['product' => Product::findOrFail($product->id)]);
     }
@@ -56,7 +59,7 @@ class ProductController extends Controller
      */
     public function edit(Product $product)
     {
-        //
+        return view('products.edit', ['product' => $product]);
     }
 
     /**
@@ -64,7 +67,19 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+        $request->validate([
+
+            'name' => 'required|min:10|max:100',
+            'description' => 'nullable|min:10',
+            // 'brand' => 'required|min:3',
+            'price' => 'required|decimal:0,2',
+            'height' => 'nullable|decimal:0,2',
+            'width' => 'nullable|decimal:0,2',
+            'weight' => 'nullable|decimal:0,2',
+            // 'category' => 'required|max:50',
+        ]);
+
+        Product::updated($request->input());
     }
 
     /**
@@ -72,6 +87,7 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        $product->delete();
+        return redirect()->route('products.index');
     }
 }
