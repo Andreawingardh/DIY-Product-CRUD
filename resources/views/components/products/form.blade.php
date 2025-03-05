3ffc1361b55361484a3a857d766f81f8a6d1a94b
Skjesper <jesperskeppstedt@gmail.com>
2025-03-05 09:38:51 +0100

    updated the form, so it now doesnt get error when trying to reach create

--- resources/views/components/products/form.blade.php
+++ resources/views/components/products/form.blade.php
@@ -7,14 +7,12 @@
 <label for='price'>Price</label>
 <input type='text' name='price' value='{{ old('price', $product->price ?? '') }}'>
 <label for='brands'>Brands</label>
-<select name='brand_id'>
+<select name='brand_id[]'>
     @foreach ($brands as $brand)
-        <option value="{{ $brand->id }}" 
-            {{ (old('brand_id', $product->brand_id) == $brand->id) ? 'selected' : '' }}>
-        {{ $brand->name }}
-      </option>
+        <option value="{{$brand->id}}">{{old('brand', $brand->name ?? '')}}</option>
     @endforeach
-  </select>
+</select>
+
 <fieldset>
 <legend for='dimensions'>Dimensions</legend>
 <label for='height'>Height</label>
@@ -27,16 +25,10 @@
 <input type='text' name='weight' value='{{ old('weight', $product->weight ?? '') }}'>
 
 <label for='categories'>Categories</label>
-<select name='category_id'>
+<select name='category_id[]'>
     @foreach ($categories as $category)
-        <option value="{{ $category->id }}" 
-                {{ (old('category_id', $product->category_id) == $category->id) ? 'selected' : '' }}>
-            {{ $category->name }}
-        </option>
+    <option value="{{ $category->id }}" {{ old('category_id') == $category->id ? 'selected' : '' }}>
+        {{ $category->name }}
+    </option>
     @endforeach
 </select>
-
-
-
-@csrf
-
