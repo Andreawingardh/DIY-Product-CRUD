5f9fdbe0eca772db70f5eeecfb0ef46d68207776
Skjesper <jesperskeppstedt@gmail.com>
2025-03-08 20:26:36 +0100

    Added navigation for show and create.
    Added x-app-layout so the navigation bar shows on all the pages

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,6 +6,8 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
+                       
+                    
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
                     </a>
                 </div>
@@ -14,6 +16,12 @@
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('products.index')" :active="request()->routeIs('products.index')">
+                        {{ __('Products') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('products.create')" :active="request()->routeIs('products.create')">
+                        {{ __('Create Product') }}
                     </x-nav-link>
                 </div>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -4,27 +4,6 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/products', [ProductController::class, 'index'])
-    ->name('products.index');
-
-Route::get('products/create', [ProductController::class, 'create'])
-    ->name('products.create');
-
-Route::post('/products/store', [ProductController::class, 'store'])
-    ->name('products.store');
-
-Route::get('/products/{product}', [ProductController::class, 'show'])
-    ->name('products.show');
-
-Route::get('/products/{product}/edit', [ProductController::class, 'edit'])
-    ->name('products.edit');
-
-Route::patch('/product/{product}', [ProductController::class, 'update'])
-    ->name('products.update');
-
-Route::delete('/products/{product}', [ProductController::class, 'destroy'])
-    ->name('products.destroy');
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -34,9 +13,19 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    // Profile routes
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    // Product routes - moved inside auth middleware
+    Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+    Route::get('products/create', [ProductController::class, 'create'])->name('products.create');
+    Route::post('/products/store', [ProductController::class, 'store'])->name('products.store');
+    Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
+    Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
+    Route::patch('/product/{product}', [ProductController::class, 'update'])->name('products.update');
+    Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
 });
 
 require __DIR__.'/auth.php';
