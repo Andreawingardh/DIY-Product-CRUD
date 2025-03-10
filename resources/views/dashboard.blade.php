251de33e039831a669768863ea6de6e82cf3edd8
Andreawingardh <andreawingardh@gmail.com>
2025-03-10 14:15:59 +0100

    Added intro text for logged in user

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,9 +3,9 @@
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Dashboard') }}
             
-
         </h2>
     </x-slot>
+    <h2>Welcome, {{ $user->name }}</h2>
 
     
 </x-app-layout>
