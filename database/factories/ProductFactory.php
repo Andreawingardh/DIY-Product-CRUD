e514c06b6e14ed5955093930b6382ff80084a88c
Andreawingardh <andreawingardh@gmail.com>
2025-03-11 09:21:47 +0100

    Added array of 100 unique product names to factory

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -19,29 +19,133 @@ class ProductFactory extends Factory
      */
     public function definition(): array
     {
+
+                
+
+        $diyHardwareProducts = [
+            // Hand Tools
+            'Claw Hammer',
+            'Screwdriver Set',
+            'Adjustable Wrench',
+            'Pliers Set',
+            'Tape Measure',
+            'Utility Knife',
+            'Level',
+            'Hacksaw',
+            'Chisel Set',
+            'Allen Wrench Set',
+            'Socket Wrench Set',
+            'Wire Cutter',
+            'Pry Bar',
+            'Clamps',
+            'Rubber Mallet',
+            
+            // Power Tools
+            'Cordless Drill',
+            'Circular Saw',
+            'Jigsaw',
+            'Orbital Sander',
+            'Angle Grinder',
+            'Router',
+            'Heat Gun',
+            'Belt Sander',
+            'Nail Gun',
+            'Electric Planer',
+            'Rotary Tool',
+            'Table Saw',
+            'Miter Saw',
+            'Power Drill Bits Set',
+            'Impact Driver',
+            
+            // Painting Supplies
+            'Paint Roller Set',
+            'Paintbrushes',
+            'Paint Tray',
+            'Drop Cloth',
+            'Paint Scraper',
+            'Caulking Gun',
+            'Paint Sprayer',
+            'Sanding Block',
+            'Painter\'s Tape',
+            'Stir Sticks',
+            
+            // Fasteners & Hardware
+            'Assorted Nails',
+            'Assorted Screws',
+            'Wall Anchors',
+            'Nuts and Bolts Kit',
+            'Picture Hangers',
+            'Wire Cable',
+            'Cabinet Hinges',
+            'Door Hinges',
+            'Drawer Slides',
+            'Cabinet Knobs',
+            'Cabinet Pulls',
+            
+            // Plumbing
+            'Pipe Wrench',
+            'Plunger',
+            'Pipe Cutter',
+            'Plumbing Tape',
+            'PVC Pipes',
+            'Pipe Fittings',
+            'Drain Snake',
+            'Toilet Repair Kit',
+            'Faucet Wrench',
+            
+            // Electrical
+            'Voltage Tester',
+            'Wire Stripper',
+            'Electrical Tape',
+            'Wire Connectors',
+            'Extension Cord',
+            'Outlet Tester',
+            'Light Switch',
+            'Electrical Outlet',
+            'Circuit Tester',
+            
+            // Garden & Outdoor
+            'Garden Trowel',
+            'Pruning Shears',
+            'Garden Hose',
+            'Leaf Rake',
+            'Garden Shovel',
+            'Lawn Mower',
+            'String Trimmer',
+            'Hedge Trimmer',
+            'Wheelbarrow',
+            'Garden Gloves',
+            
+            // Adhesives & Sealants
+            'Wood Glue',
+            'Super Glue',
+            'Epoxy',
+            'Construction Adhesive',
+            'Silicone Sealant',
+            'Duct Tape',
+            'Gorilla Tape',
+            'Weather Stripping',
+            
+            // Safety Equipment
+            'Safety Glasses',
+            'Work Gloves',
+            'Dust Mask',
+            'Ear Protection',
+            'Knee Pads',
+            'First Aid Kit',
+            'Fire Extinguisher',
+            
+            // Measuring & Layout
+            'Carpenter\'s Square',
+            'Chalk Line',
+            'Stud Finder',
+            'Digital Caliper',
+            'Laser Level',
+            'Measuring Wheels',
+            'Protractor'
+        ];
         return [
-            'name' => fake()->randomElement([
-                "Hammer", 
-                "Screwdriver Set", 
-                "Adjustable Wrench", 
-                "Cordless Drill", 
-                "Tape Measure", 
-                "Utility Knife", 
-                "Spirit Level", 
-                "Pliers Set", 
-                "Hand Saw", 
-                "Stud Finder", 
-                "Allen Wrench Set", 
-                "Chisel Set", 
-                "Safety Goggles", 
-                "Workbench", 
-                "Claw Hammer", 
-                "Wood Glue", 
-                "Sandpaper Set", 
-                "Power Sander", 
-                "Paint Roller Set", 
-                "Hot Glue Gun"
-            ]),
+            'name' => fake()->unique()->randomElement($diyHardwareProducts),
             'description' => fake()->text(100),
             'brand_id' => fake()->randomElement(Brand::pluck('id')),
             'price' => fake()->numberBetween(0, 2500),
