d805d1a54dd992419a63d91f40a08f19d7477221
Skjesper <jesperskeppstedt@gmail.com>
2025-03-12 12:21:02 +0100

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,16 +5,27 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\Brand;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class DashboardController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $products = Product::paginate(15);
-        
+
+        $products = QueryBuilder::for(Product::class)
+        ->allowedFilters(['name', 'brand_id', 'category_id'])
+        ->allowedSorts(['name', 'price', 'brand_id'])
+        ->defaultSort('name')
+        ->with(['category', 'brand'])
+        ->paginate(15)
+        ->appends($request->query());
+
         return view('dashboard', [
             'user' => Auth::user(),
-            'products' => $products // Use the paginated products
+            'products' => $products,
+            'brands' => Brand::all(), 'categories' => Category::all()
         ]);
     }
 }
