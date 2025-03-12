80bb24d35ccd6bb01136f49b634fcc321a83c968
Andreawingardh <andreawingardh@gmail.com>
2025-03-12 08:42:25 +0100

    Checked indentation in dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,13 +1,11 @@
 <!-- resources/views/dashboard.blade.php -->
 <x-app-layout>
-
-
     <section class="dashboard-header">
         <h1>Welcome, {{ $user->name }} to DIJA</h1>
-        {{-- <img class="logo" src="/images/Logo.webp"> --}}
+            {{-- <img class="logo" src="/images/Logo.webp"> --}}
     </section>
 
-    <main class="dashboard-content">
+    <section class="dashboard-content">
         <section class="dashboard-card">
             <div class="header-row">
                 <h2>Product Overview</h2>  
@@ -15,56 +13,56 @@
                     <p class="session-message">{{ session('message') }}</p>
                 @endif
             </div>
-           
-            <table class="dashboard-table">
-                <thead>
-                    <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Price</th>
-                        <th scope="col">Weight</th>
-                        <th scope="col">Height</th>
-                        <th scope="col">Width</th>
-                        <th scope="col">Category</th>
-                        @if (auth()->check() && auth()->user()->isAdmin())
-                        <th scope="col">Actions</th>
-                        @endif
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($products as $product)
-                        <tr>
-                            <td>{{ $product->id }}</td>
-                            <td>{{ $product->name }}</td>
-                            <td>${{ $product->price }}</td>
-                            <td>{{ $product->weight }}</td>
-                            <td>{{ $product->height }}</td>
-                            <td>{{ $product->width }}</td>
-                            <td>{{ $product->category->name ?? 'N/A' }}</td>
-                            <td>
-                                @if (auth()->check() && auth()->user()->isAdmin())
-                                <a href="/products/{{$product->id}}/edit">
-                                    <button class="edit-link">
-                                        Edit
-                                    </button>  
-                                </a>
-                                <form method="post" action="{{ route('products.destroy', $product) }}">
-                                    @csrf
-                                    @method('DELETE')
-                                    <input type="hidden" name="source" value="dashboard">
-                                    <button type="submit" class="delete-link" 
-                                        onclick="return confirm('Are you sure you want to delete this product?')">
-                                        Delete
-                                    </button>
-                                    @endif
-                                </form>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </section>
         
-        {{ $products->links() }}
+        <table class="dashboard-table">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">Weight</th>
+                    <th scope="col">Height</th>
+                    <th scope="col">Width</th>
+                    <th scope="col">Category</th>
+                    @if (auth()->check() && auth()->user()->isAdmin())
+                    <th scope="col">Actions</th>
+                    @endif
+                </tr>
+            </thead>
+            <tbody>
+
+                @foreach ($products as $product)
+                    <tr>
+                        <td>{{ $product->id }}</td>
+                        <td>{{ $product->name }}</td>
+                        <td>${{ $product->price }}</td>
+                        <td>{{ $product->weight }}</td>
+                        <td>{{ $product->height }}</td>
+                        <td>{{ $product->width }}</td>
+                        <td>{{ $product->category->name ?? 'N/A' }}</td>
+                        <td>
+                            @if (auth()->check() && auth()->user()->isAdmin())
+                            <a href="/products/{{$product->id}}/edit">
+                                <button class="edit-link">
+                                    Edit
+                                </button>  
+                            </a>
+                            <form method="post" action="{{ route('products.destroy', $product) }}">
+                                @csrf
+                                @method('DELETE')
+                                <input type="hidden" name="source" value="dashboard">
+                                <button type="submit" class="delete-link" 
+                                    onclick="return confirm('Are you sure you want to delete this product?')">
+                                    Delete
+                                </button>
+                                @endif
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+
+            </tbody>
+        </table>
     </section>
+        {{ $products->links() }}
 </x-app-layout>
